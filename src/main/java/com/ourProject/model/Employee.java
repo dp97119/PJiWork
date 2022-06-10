180405a1902b89b33afb6e146a1c3088703a33c4
dp97119 <dp97119@gmail.com>
2022-06-10 22:24:07 +0800

    20220610-22:23孟慧

--- src/main/java/com/ourProject/model/Employee.java
+++ src/main/java/com/ourProject/model/Employee.java
@@ -20,6 +20,114 @@ public class Employee {
 	private Set<Attendance> attendances;
 	@OneToMany(mappedBy = "employees",cascade = CascadeType.ALL)
 	private Set<Email> emails;
+	public Set<Announcement> getAnnouncements() {
+		return announcements;
+	}
+	public void setAnnouncements(Set<Announcement> announcements) {
+		this.announcements = announcements;
+	}
+	public Set<Attendance> getAttendances() {
+		return attendances;
+	}
+	public void setAttendances(Set<Attendance> attendances) {
+		this.attendances = attendances;
+	}
+	public Set<Email> getEmails() {
+		return emails;
+	}
+	public void setEmails(Set<Email> emails) {
+		this.emails = emails;
+	}
+	public Set<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+	public Set<Punch> getPunches() {
+		return punches;
+	}
+	public void setPunches(Set<Punch> punches) {
+		this.punches = punches;
+	}
+	public Set<Salaryslip> getSalaryslips() {
+		return salaryslips;
+	}
+	public void setSalaryslips(Set<Salaryslip> salaryslips) {
+		this.salaryslips = salaryslips;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public Apart getAparts() {
+		return aparts;
+	}
+	public void setAparts(Apart aparts) {
+		this.aparts = aparts;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCellphone1() {
+		return cellphone1;
+	}
+	public void setCellphone1(String cellphone1) {
+		this.cellphone1 = cellphone1;
+	}
+	public String getCellphone2() {
+		return cellphone2;
+	}
+	public void setCellphone2(String cellphone2) {
+		this.cellphone2 = cellphone2;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdm() {
+		return adm;
+	}
+	public void setAdm(String adm) {
+		this.adm = adm;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
 	@OneToMany(mappedBy = "employees",cascade = CascadeType.ALL)
 	private Set<Order> orders;
 	@OneToMany(mappedBy = "employees",cascade = CascadeType.ALL)
@@ -53,6 +161,14 @@ public class Employee {
 	private String adm;
 	@Column(name = "addr")
 	private String addr;
+	@Override
+	public String toString() {
+		return "Employee [announcements=" + announcements + ", attendances=" + attendances + ", emails=" + emails
+				+ ", orders=" + orders + ", punches=" + punches + ", salaryslips=" + salaryslips + ", empId=" + empId
+				+ ", passwd=" + passwd + ", empName=" + empName + ", photo=" + photo + ", aparts=" + aparts + ", tel="
+				+ tel + ", gender=" + gender + ", cellphone1=" + cellphone1 + ", cellphone2=" + cellphone2 + ", email="
+				+ email + ", adm=" + adm + ", addr=" + addr + "]";
+	}
 	
 
 }
