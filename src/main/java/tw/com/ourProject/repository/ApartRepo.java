5a4cbfc70132bbda0d9268e668c8c386d48e8f23
dp97119 <dp97119@gmail.com>
2022-06-14 14:31:35 +0800

    20220614-14:31孟慧：新增層級紀錄查詢

--- src/main/java/tw/com/ourProject/repository/ApartRepo.java
+++ src/main/java/tw/com/ourProject/repository/ApartRepo.java
@@ -1,7 +1,5 @@
 package tw.com.ourProject.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import tw.com.ourProject.model.Apart;
 
 @Repository
 public interface ApartRepo extends JpaRepository<Apart, Integer>{
-	List<Apart> findByapartId(Integer apartid);
+	
 }
