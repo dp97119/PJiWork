7a58899d9077984ac8e4d8e1134bf09880a18959
dp97119 <dp97119@gmail.com>
2022-06-15 16:02:53 +0800

    20220615-16:02孟慧：層級設定增加判斷部門已存在不可以新增

--- src/main/resources/static/js/approvalset/approvalset.js
+++ src/main/resources/static/js/approvalset/approvalset.js
@@ -1,15 +1,16 @@
 // 查詢
-$(function () {
-    $.ajax({
-        url: "http://localhost:8080/findApprovalset/",
-        type: "GET",
-        success: function (data) {
-            var i = 1;
-            $.each(data, function () {                
-                    var approvalsetrecord = $('<tr class="staffWord"><td class="tableStyle">'+i+`</td>
-                    <td class="tableStyle">`+this.aparts.apart+`</td>
-                    <td class="tableStyle">`+this.employees.empName+`</td>
-                    <td class="tableStyle">`+this.employee.empName+`</td>
+var approvalList = [];
+$(function() {
+	$.ajax({
+		url: "http://localhost:8080/findApprovalset/",
+		type: "GET",
+		success: function(data) {
+			var i = 1;
+			$.each(data, function() {
+				var approvalsetrecord = $('<tr class="staffWord"><td class="tableStyle">' + i + `</td>
+                    <td class="tableStyle">`+ this.aparts.apart + `</td>
+                    <td class="tableStyle">`+ this.employees.empName + `</td>
+                    <td class="tableStyle">`+ this.employee.empName + `</td>
                     <td class="tableStyle">
                         <span class="icon-input-btn">
                             <i class="glyphicon glyphicon-pencil"></i>
@@ -22,40 +23,53 @@ $(function () {
                         </span>&nbsp;&nbsp;
                     </td>
                 </tr>`);
-                approvalsetrecord.appendTo("#leavelRecord");
-                i++;
-            })
-        }
-    })
+				approvalsetrecord.appendTo("#leavelRecord");
+				approvalList[i-1] = this.aparts.apartId;
+				i++;
+			})
+		}
+	})
 })
 
-
 // 新增
-$("#sendbtn").on("click",function(){
-    console.log("OK");
-    if($("#aparts").find("option:selected").val() == "" || $("#reviewerone").find("option:selected").val() == "" || $("#reviewertwo").find("option:selected").val() == "" ){
-        confirm("欄位不可為空白");
-    }else{
-        var insertSet = JSON.stringify([{
-            apartId: $("#aparts").find("option:selected").val(),
-            fisrtApproval:$("#reviewerone").find("option:selected").val(),
-            secondApproval: $("#reviewertwo").find("option:selected").val()
-        }]);
+$("#sendbtn").on("click", function() {
+	if ($("#aparts").find("option:selected").val() == "" || $("#reviewerone").find("option:selected").val() == "" || $("#reviewertwo").find("option:selected").val() == "") {
+		confirm("欄位不可為空白");
+	} else {
+		var j = false;
+		for (var i = 0; i < approvalList.length; i++) {
+			if ($("#aparts").find("option:selected").val() == approvalList[i]){
+				j = true;
+				break;
+			}
+		}
+		if (j == false) {
+			var insertSet = JSON.stringify([{
+				apartId: $("#aparts").find("option:selected").val(),
+				fisrtApproval: $("#reviewerone").find("option:selected").val(),
+				secondApproval: $("#reviewertwo").find("option:selected").val()
+			}]);
 
-        
-        $.ajax({
-            url: "http://localhost:8080/saveApprovalset/",
-            type: "POST",
-            data: insertSet,
-            contentType:"application/json",
-            success:function(){
-                var yes = confirm("確認完成");
-                if (yes) {
-                    window.location.href = './CMS_2Leaverecorded.html';
-                }
-            }
-        })
-    }
+			$.ajax({
+				url: "http://localhost:8080/saveApprovalset/",
+				type: "POST",
+				data: insertSet,
+				contentType: "application/json",
+				success: function() {
+					var yes = confirm("確認完成");
+					if (yes) {
+						window.location.href = './CMS_6_4_1.html';
+					}
+				}
+			})
+		} else {
+			var yes = confirm("資料已存在");
+			if (yes) {
+				window.location.href = './CMS_6_4_1.html';
+			}
+		}
+		
+	}
 
 })
 
