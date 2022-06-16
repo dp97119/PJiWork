0dba845d114bbe8c49c5dd00109f120085e6d4a4
dp97119 <dp97119@gmail.com>
2022-06-16 21:03:16 +0800

    20220616-21:02孟慧：調整行事曆前端呈現以及樣式判斷

--- src/main/java/tw/com/ourProject/model/Calendar.java
+++ src/main/java/tw/com/ourProject/model/Calendar.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class Calendar {
 	@Id
 	@Column(name = "calendarid")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer calendarId;
 	
 	@Column(name = "calendartype",columnDefinition="ENUM('個人','部門','公司')")
@@ -32,5 +35,59 @@ public class Calendar {
 	
 	@Column(name = "allday",columnDefinition="ENUM('Y','N')")
 	private String allDay;
+
+	public Integer getCalendarId() {
+		return calendarId;
+	}
+
+	public void setCalendarId(Integer calendarId) {
+		this.calendarId = calendarId;
+	}
+
+	public String getCalendarType() {
+		return calendarType;
+	}
+
+	public void setCalendarType(String calendarType) {
+		this.calendarType = calendarType;
+	}
+
+	public String getCalendarTitle() {
+		return calendarTitle;
+	}
+
+	public void setCalendarTitle(String calendarTitle) {
+		this.calendarTitle = calendarTitle;
+	}
+
+	public java.util.Date getEventStart() {
+		return eventStart;
+	}
+
+	public void setEventStart(java.util.Date eventStart) {
+		this.eventStart = eventStart;
+	}
+
+	public java.util.Date getEventEnd() {
+		return eventEnd;
+	}
+
+	public void setEventEnd(java.util.Date eventEnd) {
+		this.eventEnd = eventEnd;
+	}
+
+	public String getAllDay() {
+		return allDay;
+	}
+
+	public void setAllDay(String allDay) {
+		this.allDay = allDay;
+	}
+
+	@Override
+	public String toString() {
+		return "Calendar [calendarId=" + calendarId + ", calendarType=" + calendarType + ", calendarTitle="
+				+ calendarTitle + ", eventStart=" + eventStart + ", eventEnd=" + eventEnd + ", allDay=" + allDay + "]";
+	}
 	
 }
