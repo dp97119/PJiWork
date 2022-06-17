9e295577f66d60c0faa9ef53dc3df547a3797d15
Mina Lin <cathy00272007@gmail.com>
2022-06-17 11:39:04 +0800

    0617 Zih

--- src/main/resources/static/js/CMS6_4approvalset/approvalset.js
+++ src/main/resources/static/js/CMS6_4approvalset/approvalset.js
@@ -1,30 +1,31 @@
 // 查詢
 var approvalList = [];
-$(function() {
+$(function () {
 	$.ajax({
 		url: "http://localhost:8080/findApprovalset/",
 		type: "GET",
-		success: function(data) {
+		success: function (data) {
 			var i = 1;
-			$.each(data, function() {
-				var approvalsetrecord = $(`<tr class="staffWord" id="${this.approvalSetId}"><td class="tableStyle"> ${i} </td>
-                    <td class="tableStyle"> ${this.aparts.apart}</td>
-                    <td class="tableStyle">${this.employees.empName} </td>
-                    <td class="tableStyle"> ${this.employee.empName} </td>
-                    <td class="tableStyle">
-                        <span class="icon-input-btn">
-                            <i class="glyphicon glyphicon-pencil"></i>
-                            <input type="button" value="修改" class="function"
-							onClick="window.location.href='./CMS_6_4_2.html';editbtn( ${i})"/>
-                        </span>&nbsp;&nbsp;
-                        <span class="icon-input-btn">
-                            <i class="glyphicon glyphicon-trash"></i>
-                            <input type="button" value="刪除" class="function" onClick="deletebtn( ${i})"/>
-                        </span>&nbsp;&nbsp;
-                    </td>
-                </tr>`);
+			$.each(data, function () {
+				var approvalsetrecord =
+					$(`<tr class="staffWord" id="${this.approvalSetId}">
+						<td class="tableStyle"> ${i} </td>
+                    	<td class="tableStyle"> ${this.aparts.apart}</td>
+                    	<td class="tableStyle">${this.employees.empName} </td>
+                    	<td class="tableStyle"> ${this.employee.empName} </td>
+                    	<td class="tableStyle">
+                        	<span class="icon-input-btn">
+                            	<i class="glyphicon glyphicon-pencil"></i>
+                            	<input type="button" value="修改" class="function" onClick="window.location.href='./CMS_6_4_2.html';editbtn( ${i})"/>
+                        	</span>&nbsp;&nbsp;
+                        	<span class="icon-input-btn">
+                            	<i class="glyphicon glyphicon-trash"></i>
+                            	<input type="button" value="刪除" class="function" onClick="deletebtn( ${i})"/>
+                        	</span>&nbsp;&nbsp;
+                    	</td>
+                	</tr>`);
 				approvalsetrecord.appendTo("#leavelRecord");
-				approvalList[i-1] = this.aparts.apartId;
+				approvalList[i - 1] = this.aparts.apartId;
 				i++;
 			})
 		}
@@ -34,20 +35,22 @@ $(function() {
 
 
 // 新增
-$("#sendbtn").on("click", function() {
+$("#sendbtn").on("click", function () {
 	if ($("#aparts").find("option:selected").val() == "" || $("#reviewerone").find("option:selected").val() == "" || $("#reviewertwo").find("option:selected").val() == "") {
+
 		confirm("欄位不可為空白");
+
 	} else {
 		var j = false;
 		for (var i = 0; i < approvalList.length; i++) {
-			if ($("#aparts").find("option:selected").val() == approvalList[i]){
+			if ($("#aparts").find("option:selected").val() == approvalList[i]) {
 				j = true;
 				break;
 			}
 		}
 		if (j == false) {
 			var insertSet = JSON.stringify([{
-				apartId: $("#aparts").find("option:selected").val(),
+				apartId: $("#aparts").find("this.approvalSetId").val(),
 				fisrtApproval: $("#reviewerone").find("option:selected").val(),
 				secondApproval: $("#reviewertwo").find("option:selected").val()
 			}]);
@@ -57,7 +60,7 @@ $("#sendbtn").on("click", function() {
 				type: "POST",
 				data: insertSet,
 				contentType: "application/json",
-				success: function() {
+				success: function () {
 					var yes = confirm("確認完成");
 					if (yes) {
 						window.location.href = './CMS_6_4_1.html';
@@ -70,17 +73,22 @@ $("#sendbtn").on("click", function() {
 				window.location.href = './CMS_6_4_1.html';
 			}
 		}
-		
+
 	}
 
 })
 
 
 // 修改
-function editbtn(i){
-	console.log("hihi");
-    console.log(document.getElementsByTagName("tr")[i].getAttribute('id'));
-	
+function editbtn(i) {
+	// var editSet = JSON.stringify([{
+	// 	approvalSetId: document.getElementsByTagName("tr")[i].getAttribute('id')
+	// }]);
+	// console.log(editSet);
+
+	// console.log("hihi");
+	// console.log(document.getElementsByTagName("tr")[i].getAttribute('id'));
+
 }
 
 
@@ -88,8 +96,30 @@ function editbtn(i){
 // 刪除
 
 // getAttribute回傳屬性值
-function deletebtn(i){
-	console.log("haha");
-    console.log(document.getElementsByTagName("tr")[i].getAttribute('id'));
 
+function deletebtn(i) {
+	var deleteSet = JSON.stringify([{
+		approvalSetId: document.getElementsByTagName("tr")[i].getAttribute('id')
+	}]);
+
+	$.ajax({
+		url: "http://localhost:8080/deleteApprovalset/",
+		type: "DELETE",
+		data: deleteSet,
+		contentType: "application/json",
+		success: function () {
+			var yes = confirm("刪除成功");
+			if (yes) {
+				window.location.href = './CMS_6_4_1.html';
+			}
+			console.log(deleteSet);
+		}
+	})
+
+
+
+
+
+	// console.log("haha");
+	// console.log(document.getElementsByTagName("tr")[i].getAttribute('id'));
 }
