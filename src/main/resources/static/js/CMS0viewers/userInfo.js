5b22c73e49a5a746fc4446321770a5a08b344531
Mina Lin <cathy00272007@gmail.com>
2022-06-18 15:28:38 +0800

    20220618個人資訊修改

--- src/main/resources/static/js/CMS0viewers/userInfo.js
+++ src/main/resources/static/js/CMS0viewers/userInfo.js
@@ -1,7 +1,7 @@
 // 中間個人資訊
 $(function () {
 
-    var usertoken = JSON.stringify({"userToken": getCookie() });
+    var usertoken = JSON.stringify({ "userToken": getCookie() });
 
     $.ajax({
         type: "POST",
@@ -9,10 +9,10 @@ $(function () {
         data: usertoken,
         contentType: 'application/json',
         dataType: 'json',
-        success: function(data){
+        success: function (data) {
             userApart.innerText = data.apart;
-            userNumber.innerText = data.empId; 
-            userName.innerText = data.empName;  
+            userNumber.innerText = data.empId;
+            userName.innerText = data.empName;
         }
     })
 })
@@ -20,27 +20,52 @@ $(function () {
 function getCookie() {
     var cookie = document.cookie.split('=');
     return cookie[1].toString();
- }
+}
 
-// 修改information(POST) 
+// 修改information(PUT) 
 $(function () {
-
-    var usertoken = JSON.stringify({"userToken": getCookie() });
-
+    var usertoken = JSON.stringify({ "userToken": getCookie() });
     $.ajax({
         type: "POST",
         url: "/emp/getUserInfo",
         data: usertoken,
         contentType: 'application/json',
         dataType: 'json',
-        success: function(data){
+        success: function (data) {        
             $("#inUserName").val(data.empName);
             $("#inUserPwd").val(data.empPwd);
             $("#inUserTel").val(data.empTel);
             $("#inUserPhone").val(data.empPhone);
-            $("#inUserEmail").val(empEmail);
-            $("#inUserAddr").val(empAddr);
+            $("#inUserEmail").val(data.empEmail);
+            $("#inUserAddr").val(data.empAddr);
         }
     })
+
+
+
 })
+
+function informationBtn() {
+	console.log("OK")
+    var usertoken = JSON.stringify({"userToken": getCookie(),   
+    "empName": $("#inUserName").val(),
+    "empPwd": $("#inUserPwd").val(),
+    "empTel": $("#inUserTel") .val(),
+    "empPhone": $("#inUserPhone").val(),
+    "empEmail": $("#inUserEmail").val(),
+    "empAddr": $("#inUserAddr").val(),    
+    });
+    console.log(usertoken);
+    $.ajax({
+        type: "PUT",
+        url: "/emp/updateUserInfo",
+        data: usertoken,
+        contentType:'application/json',
+        success: function () {
+            console.log($("#inUserName").val());
+            alert("修改成功")
+        }
+    })
+
+}
 
