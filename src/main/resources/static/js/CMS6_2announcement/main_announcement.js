66de3ead01b3fb2403a215b0aae1630304cf35c9
Mina Lin <cathy00272007@gmail.com>
2022-06-19 00:27:55 +0800

    20220619 公告

--- src/main/resources/static/js/CMS6_2announcement/main_announcement.js
+++ src/main/resources/static/js/CMS6_2announcement/main_announcement.js
@@ -6,6 +6,7 @@ $(function () {
         url: "http://localhost:8080/showAnnouncement/",
         type: "GET",
         success: function (data) {
+            // var i = -1;
             $.each(data, function () {
                 var NowDate = new Date();
                 var yy = NowDate.getFullYear();
@@ -15,10 +16,41 @@ $(function () {
                 // console.log(today);
 
                 if (today < this.removed) {
-                    var announcementRecord = $(`<li id="notice">` + this.title + `</li>`);
-                    $("#notice").before(announcementRecord);
+                    var announcementRecord = $(`<li id="${this.announceId}" class="noticeContent" onclick="openbtn(${this.announceId})">${this.title}</li>`);
+                    $("#mainAnnouncement").after(announcementRecord);
+                    // i++;
+
+                }else{
+                    // console.log("下架了")
                 }
+
             })
         }
+
     })
 })
+
+
+
+// 公告第二層彈跳視窗
+function openbtn(i) {
+    console.log("OK");
+    var openNumber = JSON.stringify([{
+        announceId: document.getElementById(`${i}`).innerText
+    }]);
+    console.log(openNumber);
+
+}
+
+
+
+// $("#notice").on("click", function () {
+    // var annTwo = JSON.stringify([{
+    //     announceId: document.getElementsByTagName("li")[i].getAttribute('value')
+    // }]);
+    // console.log(annTwo);
+    // currentIndex = idx;
+    // $("#titleTextBox").prop("value", newsList[idx].title);
+    // $("#ymdTextBox").prop("value", newsList[idx].ymd);
+    // $("#newsModal").modal({ backdrop: "static" });
+// })
