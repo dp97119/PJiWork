26de4ad9c3b9b20e3d19ff917a0c9420c7aa356c
Mina Lin <cathy00272007@gmail.com>
2022-06-19 15:06:37 +0800

    20220619 出勤系統部門增刪修查已完成

--- src/main/resources/static/js/CMS6_4approvalset/approvalset.js
+++ src/main/resources/static/js/CMS6_4approvalset/approvalset.js
@@ -16,7 +16,7 @@ $(function () {
                     	<td class="tableStyle">
                         	<span class="icon-input-btn">
                             	<i class="glyphicon glyphicon-pencil"></i>
-                            	<input type="button" value="修改" class="function" onClick="editbtn( ${i})"; />
+                            	<input type="button" value="修改" class="function editOpen" onClick="editbtn( ${i})"; />
                         	</span>&nbsp;&nbsp;
                         	<span class="icon-input-btn">
                             	<i class="glyphicon glyphicon-trash"></i>
@@ -59,89 +59,18 @@ $("#sendbtn").on("click", function () {
 				contentType: "application/json",
 				success: function () {
 					if (confirm("確認完成")) {
-						window.location.href = './CMS_6_4_1.html';
+						parent.document.getElementById("changeApart").style.visibility = "hidden";
+						// window.location.href = './CMS_6_4_1.html';
+						parent.location.reload();
 					}
 				}
 			})
 		} else {
 			if (confirm("資料已存在")) {
-				window.location.href = './CMS_6_4_1.html';
+				// window.location.href = './CMS_6_4_1.html';
+				parent.changeApart.close();
 			}
 		}
 	}
 })
-
-// 修改
-function editbtn(i) {
-	var findId = JSON.stringify({
-		approvalSetId: document.getElementsByTagName("tr")[i].getAttribute('id')
-	});
-	$(function () {
-		console.log(findId)
-		console.log("--------")
-		$.ajax({
-			url: "http://localhost:8080/Approval/dataresponse/",
-			type: "POST",
-			data: findId,
-			contentType: "application/json",
-			success: function (data) {
-				console.log(findId)
-				console.log(data.firstApprovalId);
-				console.log(data.secondApprovalId);
-				window.location.href = './CMS_6_4_2.html';
-				$("approvalsetid").val(data.approvalSetId)
-				$('#aparts').find(`option[value=${data.apartId}]`).prop('selected',true);  //可以抓
-				$("#reviewerone").find(`option[value=${data.firstApprovalId}]`).prop('selected',true);
-				$("#reviewertwo").find(`option[value=${data.secondApprovalId}]`).prop('selected',true);
-				$('#aparts').prop('disabled',true); //可以鎖
-				console.log("確認收到")
-			}
-		})
-	})
-	
-}
-	// var editSet = JSON.stringify([{
-	// 	approvalSetId: document.getElementsByTagName("tr")[i].getAttribute('id'),
-	// 	fisrtApproval: $("#reviewerone").find("option:selected").val(),
-	// 	secondApproval: $("#reviewertwo").find("option:selected").val()
-	// }]);
-	// console.log(editSet);
-	// $.ajax({
-	// 	url: "http://localhost:8080/Approvalset/update/",
-	// 	type: "PUT",
-	// 	data: editSet,
-	// 	contentType: "application/json",
-	// 	success: function () {
-	// 		console.log(editSet);
-	// 		if (confirm("修改成功")) {
-	// 			window.location.href = './CMS_6_4_1.html';
-	// 		}
-	// 	}
-	// })
-
-// }
-
-
-
-// 刪除
-
-// getAttribute回傳屬性值
-
-function deletebtn(i) {
-	var deleteSet = JSON.stringify([{
-		approvalSetId: document.getElementsByTagName("tr")[i].getAttribute('id')
-	}]);
-
-	$.ajax({
-		url: "http://localhost:8080/Approvalset/delete/",
-		type: "DELETE",
-		data: deleteSet,
-		contentType: "application/json",
-		success: function () {
-			if (confirm("刪除成功")) {
-				window.location.href = './CMS_6_4_1.html';
-			}
-		}
-	})
-}
 
