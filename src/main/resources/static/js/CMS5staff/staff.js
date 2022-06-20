ec5ec16b61aac4491735731e62be08b2fe0b4e76
Mina Lin <cathy00272007@gmail.com>
2022-06-21 02:06:46 +0800

    20220621 staff

--- src/main/resources/static/js/CMS5staff/staff.js
+++ src/main/resources/static/js/CMS5staff/staff.js
@@ -1,32 +1,67 @@
+// 紀錄
+$(function () {
+    $.ajax({
+        url: "http://localhost:8080/emp/findAll",
+        type: "GET",
+        success: function (data) {
+            console.log(data);
+            var i = 1;
+            $.each(data, function () {
+                var staffAll = $(`<tr class="staffTr">
+                    <td class="tableStyle">${i}</td>
+                    <td class="tableStyle">${this.aparts.apart}</td>
+                    <td class="tableStyle" id="empNames">${this.empName}</td>
+                    <td class="tableStyle">${this.tel}</td>
+                    <td class="tableStyle">${this.cellphone1}</td>
+                    <td class="tableStyle">${this.email}</td>
+                </tr>`);
+
+                staffAll.appendTo("#staffRecord");
+                i++;
+            })
+        }
+    })
+})
+
+
 // 查詢
-// $(function () {
-//     $.ajax({
-//         url: "http://localhost:8080/findApprovalset/",
-//         type: "GET",
-//         success: function (data) {
-//             var i = 1;
-//             $.each(data, function () {                
-//                     var approvalsetrecord = $('<tr class="staffWord"><td class="tableStyle">'+i+`</td>
-//                     <td class="tableStyle">`+this.aparts.apart+`</td>
-//                     <td class="tableStyle">`+this.aparts.apart+`</td>
-//                     <td class="tableStyle">`+this.aparts.apart+`</td>
-//                     <td class="tableStyle">
-//                         <span class="icon-input-btn">
-//                             <i class="glyphicon glyphicon-pencil"></i>
-//                             <input type="button" value="修改" class="function"
-//                                 onClick="window.location.href='./CMS_6_4_2.html';"/>
-//                         </span>&nbsp;&nbsp;
-//                         <span class="icon-input-btn">
-//                             <i class="glyphicon glyphicon-trash"></i>
-//                             <input type="button" value="刪除" class="function"/>
-//                         </span>&nbsp;&nbsp;
-//                     </td>
-//                 </tr>`);
-                
-//                 approvalsetrecord.appendTo("#leavelRecord");
-//                 i++;
-//                 console.log(data);
-//             })
-//         }
-//     })
-// })
+$("#staffSearchbtn").on("click", function () {
+    // console.log("OK")
+    $(".staffTr").empty();
+    $.ajax({
+        url: "http://localhost:8080/emp/findAll",
+        type: "GET",
+        success: function (data) {
+            var k = 1;
+            $.each(data, function () {
+                if (this.aparts.apartId == $("#aparts").val() || this.empName == $("#empNameIn").val()) {
+                    var staffAllS2 = $(`<tr class="staffTr">
+                        <td class="tableStyle">${k}</td>
+                        <td class="tableStyle">${this.aparts.apart}</td>
+                        <td class="tableStyle">${this.empName}</td>
+                        <td class="tableStyle">${this.tel}</td>
+                        <td class="tableStyle">${this.cellphone1}</td>
+                        <td class="tableStyle">${this.email}</td>
+                    </tr>`);
+
+                    staffAllS2.appendTo("#staffRecord");
+                    k++;
+                }else if ($("#aparts").val() == "" && $("#empNameIn").val() == "") {
+                    var staffAllS3 = $(`<tr class="staffTr">
+                        <td class="tableStyle">${k}</td>
+                        <td class="tableStyle">${this.aparts.apart}</td>
+                        <td class="tableStyle">${this.empName}</td>
+                        <td class="tableStyle">${this.tel}</td>
+                        <td class="tableStyle">${this.cellphone1}</td>
+                        <td class="tableStyle">${this.email}</td>
+                    </tr>`);
+
+                    staffAllS3.appendTo("#staffRecord");
+                    k++;
+                }
+            })
+
+        }
+    })
+})
+
