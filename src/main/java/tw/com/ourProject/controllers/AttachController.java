bf974379f52d367aa0c974c4054d83828d7e7775
dp97119 <dp97119@gmail.com>
2022-06-22 01:38:19 +0800

    20220622-01:37孟慧：更新AttachController加上RestController

--- src/main/java/tw/com/ourProject/controllers/AttachController.java
+++ src/main/java/tw/com/ourProject/controllers/AttachController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tw.com.ourProject.model.Attach;
 import tw.com.ourProject.service.AttachService;
 
+@RestController
 public class AttachController {
 	@Autowired
 	public AttachService attachService;
