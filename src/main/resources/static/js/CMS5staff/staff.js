696bb35836e52c656f3082e4b09cca3a3ebe1cca
Mina Lin <cathy00272007@gmail.com>
2022-06-22 09:43:45 +0800

    20220622 員工通訊錄修改完成

--- src/main/resources/static/js/CMS5staff/staff.js
+++ src/main/resources/static/js/CMS5staff/staff.js
@@ -4,16 +4,27 @@ $(function () {
         url: "http://localhost:8080/emp/findAll",
         type: "GET",
         success: function (data) {
-            console.log(data);
             var i = 1;
             $.each(data, function () {
+                var phone1 = this.tel;
+                var cell = this.cellphone1;
+                var email1 = this.email;
+                if (phone1 == null) {
+                    phone1 = "";
+                }
+                if (cell == null) {
+                    cell = "";
+                }
+                if (email1 == null) {
+                    email1 = "";
+                }
                 var staffAll = $(`<tr class="staffTr">
                     <td class="tableStyle">${i}</td>
                     <td class="tableStyle">${this.aparts.apart}</td>
                     <td class="tableStyle" id="empNames">${this.empName}</td>
-                    <td class="tableStyle">${this.tel}</td>
-                    <td class="tableStyle">${this.cellphone1}</td>
-                    <td class="tableStyle">${this.email}</td>
+                    <td class="tableStyle">${phone1}</td>
+                    <td class="tableStyle">${cell}</td>
+                    <td class="tableStyle">${email1}</td>
                 </tr>`);
 
                 staffAll.appendTo("#staffRecord");
@@ -34,14 +45,26 @@ $("#staffSearchbtn").on("click", function () {
         success: function (data) {
             var i = 1;
             $.each(data, function () {
-                if (this.aparts.apartId == $("#aparts").val() || this.empName == $("#empNameIn").val()  || ($("#aparts").val() == "" && $("#empNameIn").val() == "")) {
+                var phone1 = this.tel;
+                var cell = this.cellphone1;
+                var email1 = this.email;
+                if (phone1 == null) {
+                    phone1 = "";
+                }
+                if (cell == null) {
+                    cell = "";
+                }
+                if (email1 == null) {
+                    email1 = "";
+                }
+                if (this.aparts.apartId == $("#aparts").val() || this.empName == $("#empNameIn").val() || ($("#aparts").val() == "" && $("#empNameIn").val() == "")) {
                     var staffAllS2 = $(`<tr class="staffTr">
                         <td class="tableStyle">${i}</td>
                         <td class="tableStyle">${this.aparts.apart}</td>
                         <td class="tableStyle">${this.empName}</td>
-                        <td class="tableStyle">${this.tel}</td>
-                        <td class="tableStyle">${this.cellphone1}</td>
-                        <td class="tableStyle">${this.email}</td>
+                        <td class="tableStyle">${phone1}</td>
+                        <td class="tableStyle">${cell}</td>
+                        <td class="tableStyle">${email1}</td>
                     </tr>`);
 
                     staffAllS2.appendTo("#staffRecord");
